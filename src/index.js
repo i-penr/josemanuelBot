ae570f5766a13f15727f1f6f1038b61e16743a3a
Zatapo <ismael.penrodriguez@alumnos.upm.es>
2022-05-21 11:44:54 +0200

    reduced number of lines in index.js client ready event

--- src/index.js
+++ src/index.js
@@ -33,22 +33,9 @@ client.on('voiceStateUpdate', (oldVs, newVs) => {
 
 client.on('ready', () => {
 
-  // EMT login
-  fetch(emtconfig.url + 'mobilitylabs/user/login/', { method: 'GET', headers: emtconfig.headers })
-    .then((res) => {
-      if (!res.ok) {
-        console.log('There was an error connecting to de MobilityLabs API' + res.status);
-      } else { return res.json(); }
-    })
-    .then((json) => {
-      if (!json) { return; }
-      emtconfig.headers.accessToken = json.data[0].accessToken;
-      console.log('Successfully logged in to EMT API with token: ' + emtconfig.headers.accessToken);
-    })
-    .catch((err) => console.error(err));
+  emtAPILogin();
 
   client.user.setPresence({ activities:  [{ name: 'Hawaii: Part II', type: 'LISTENING' }] });
-
 
   commandFiles.forEach((file) => {
     const command = require(`./commands/${file}`);
@@ -59,8 +46,7 @@ client.on('ready', () => {
     }
   });
 
-  const gatoChannel = client.channels.cache.get('595718884288495759');
-  schedule.scheduleJob('5 45 * * * *', () => { gatoPicha(gatoChannel); });
+  schedule.scheduleJob('5 45 * * * *', () => { gatoPicha(); });
 
   console.log('Connected');
 
@@ -90,7 +76,8 @@ client.on('messageCreate', (msg) => {
 });
 
 
-function gatoPicha(gatoChannel) {
+function gatoPicha() {
+  const gatoChannel = client.channels.cache.get('595718884288495759');
   const gatoChance = Math.floor(Math.random() * 10000) + 1;
 
   console.log(`[gatoconlapichatiesa] gatoChance = ${gatoChance}`);
@@ -116,6 +103,21 @@ function messageCooldown(user, cooldownTime) {
   setTimeout(() => {
     talkedRecently.delete(user);
   }, cooldownTime);
+}
+
+function emtAPILogin() {
+  fetch(emtconfig.url + 'mobilitylabs/user/login/', { method: 'GET', headers: emtconfig.headers })
+    .then((res) => {
+      if (!res.ok) {
+        console.log('There was an error connecting to de MobilityLabs API' + res.status);
+      } else { return res.json(); }
+    })
+    .then((json) => {
+      if (!json) { return; }
+      emtconfig.headers.accessToken = json.data[0].accessToken;
+      console.log('Successfully logged in to EMT API with token: ' + emtconfig.headers.accessToken);
+    })
+    .catch((err) => console.error(err));
 }
 
 client.login(config.token);
