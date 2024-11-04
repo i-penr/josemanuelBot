cd2a453d119dd6b97e930efe662af2fe905d28ca
i-penr <ismaelpenalvarodriguez@gmail.com>
2024-11-05 00:24:36 +0100

    Updated risk cmd

--- src/commands/risk.js
+++ src/commands/risk.js
@@ -7,15 +7,74 @@ module.exports = {
 
   execute(msg, args) {
     args.shift();
-    const roll = Math.ceil(Math.random() * 9999) + 1;
+    const roll = msg.id;
     const text = args.join(' ');
     const bonusStr = /\(([^)]+)\)/.exec(args.at(-1));
-    const bonus = bonusStr ? Number(bonusStr[1]) : 0;
+    const bonus = bonusStr && !isNaN(Number(bonusStr[1])) ? Number(bonusStr[1]) : 0;
 
     if (text.length === 0) {
       return msg.reply('Falta la acción, colega...');
     }
 
-    msg.reply(`${text} \n\`roll\`: **${roll}**\n\`bonus\`: **${bonus}**\n**${roll}** + **${bonus}** = **${roll + bonus}**`);
+    const rollObj = getRollType(roll);
+
+    msg.reply(`${text} \n${rollObj.rollMsg}\n\`roll\`: **${roll}**\n\`bonus\`: **${bonus}**\n**${rollObj.territories}** + **${bonus}** = **${rollObj.territories + bonus}**`);
   },
 };
+
+function getRollType(roll) {
+  const last4 = roll.slice(-4);
+  const last = parseInt(last4.slice(-1));
+
+  const rollTypes = [
+    {
+      condition: () => parseInt(last4) % 1111 === 0,
+      type: 'QUADS',
+      territories: 15,
+      extraAction: 'extra turn + build a new city',
+    },
+    {
+      condition: () => parseInt(last4.slice(-3)) % 111 === 0,
+      type: 'TRIPS',
+      territories: 15,
+      extraAction: 'extra turn + build a new city',
+    },
+    {
+      condition: () => parseInt(last4.slice(-2)) % 11 === 0,
+      type: 'DUBS',
+      territories: 12,
+      extraAction: 'extra turn',
+    },
+    {
+      condition: () => last === 0,
+      type: 'Epic 0',
+      territories: 12,
+      extraAction: 'extra turn',
+    },
+    {
+      condition: () => last >= 7,
+      type: 'Golden Roll',
+      territories: 9,
+    },
+    {
+      condition: () => last >= 4,
+      type: 'Silver Roll',
+      territories: 6,
+    },
+    {
+      condition: () => last >= 1,
+      type: 'Bronze Roll',
+      territories: 3,
+    },
+  ];
+
+  const result = rollTypes.find(type => type.condition());
+
+  if (result) {
+    const { type, territories, extraAction } = result;
+    const rollMsg = `***${type}***\n${territories} territories${extraAction ? ` + ${extraAction}` : ''}`;
+    return { rollMsg, territories };
+  }
+
+  return { rollMsg: 'Invalid Roll', territories: 0 };
+}
