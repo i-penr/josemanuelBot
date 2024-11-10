8b7e50caeae23644ec558493c4ecd73e9c6cc016
i-penr <ismaelpenalvarodriguez@gmail.com>
2024-11-10 01:26:52 +0100

    Added risk command interaction

--- src/interactions/juegos/risk.js
+++ src/interactions/juegos/risk.js
@@ -1,12 +1,45 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle } = require("discord.js");
+const { getBonusFromText, generateRollString } = require("../../utils/riskUtils");
 
 const interaction = {
     data: new SlashCommandBuilder()
-            .setName('risk')
-            .setDescription('EL PUTO DADO DEL RISK AHORA EN SLASH'),
+        .setName('risk')
+        .setDescription('EL PUTO DADO DEL RISK AHORA EN SLASH'),
     async execute(interaction) {
-        await interaction.reply(`Pong!\nLatency: ${Date.now() - interaction.createdTimestamp} ms`);
+        const roll = interaction.id;
+        await promptModal(interaction);
+
+        const filter = (interaction) => interaction.customId === 'rollModal';
+        interaction.awaitModalSubmit({ filter, time: 60000 })
+            .then(interaction => {
+                const text = interaction.fields.getTextInputValue('actionInput')
+                const bonus = getBonusFromText(text);
+
+                if (text.length === 0) {
+                    return interaction.reply({ content: 'Falta la acción, colega...', ephemeral: true });
+                }
+
+                interaction.reply({ content: generateRollString(text, bonus, roll), ephemeral: true });
+            })
+            .catch(console.error);
     },
 }
 
 module.exports = { interaction };
+
+async function promptModal(interaction) {
+    const modal = new ModalBuilder()
+        .setCustomId('rollModal')
+        .setTitle(`Roll de ${interaction.user.username}`);
+
+    const actionInput = new TextInputBuilder()
+        .setCustomId('actionInput')
+        .setLabel("Introduce la acción del rol")
+        .setPlaceholder('Me cago encima... spills me meo (+1).')
+        .setStyle(TextInputStyle.Paragraph);
+
+    const actionRow = new ActionRowBuilder().addComponents(actionInput);
+    modal.addComponents(actionRow);
+
+    await interaction.showModal(modal);
+}
---
+++ src/utils/riskUtils.js
@@ -0,0 +1,96 @@
+function getBonusFromText(text) {
+    const parentheses = (text.match(/\(([^)]+)\)/g))?.map((e) => removeParenthesesFromString(e)).filter((e) => !isNaN(parseInt(e)));
+    return parentheses && parentheses.length > 0 ? parseInt(parentheses.at(-1)) : 0;
+}
+
+function getRollType(roll) {
+    const last4 = roll.slice(-4);
+    const last = parseInt(last4.slice(-1));
+
+    const rollTypes = [
+        {
+            condition: () => parseInt(last4) % 1111 === 0,
+            type: 'QUADS',
+            territories: 15,
+            extraAction: 'extra turn + build a new city',
+        },
+        {
+            condition: () => parseInt(last4.slice(-3)) % 111 === 0,
+            type: 'TRIPS',
+            territories: 15,
+            extraAction: 'extra turn + build a new city',
+        },
+        {
+            condition: () => parseInt(last4.slice(-2)) % 11 === 0,
+            type: 'DUBS',
+            territories: 12,
+            extraAction: 'extra turn',
+        },
+        {
+            condition: () => last === 0,
+            type: 'EPIC 0',
+            territories: 12,
+            extraAction: 'extra turn',
+        },
+        {
+            condition: () => last >= 7,
+            type: 'Golden Roll',
+            territories: 9,
+        },
+        {
+            condition: () => last >= 4,
+            type: 'Silver Roll',
+            territories: 6,
+        },
+        {
+            condition: () => last >= 1,
+            type: 'Bronze Roll',
+            territories: 3,
+        },
+    ];
+
+    const result = rollTypes.find(type => type.condition());
+
+    if (result) {
+        const { type, territories, extraAction } = result;
+        const rollMsg = `***${type}***\n${territories} territories${extraAction ? ` + ${extraAction}` : ''}`;
+        return { rollMsg, territories };
+    }
+
+    return { rollMsg: 'Invalid Roll', territories: 0 };
+}
+
+function generateRollString(text, bonus, roll) {
+    const rollObj = getRollType(roll);
+
+    return `${text} \n${rollObj.rollMsg}\n\`roll\`: **${roll}**\n\`bonus\`: **${bonus}**\n**${rollObj.territories}** + **${bonus}** = **${rollObj.territories + bonus}**`;
+}
+
+function removeParenthesesFromString(str) {
+    return str.replace(/[()]/g, '')
+}
+
+function listenForMessageEdits(client, msg, reply) {
+    const callback = (oldMessage, newMessage) => {
+        if (newMessage.id === msg.id) {
+            const text = newMessage.content.substr(newMessage.content.indexOf(" ") + 1);
+            const bonus = getBonusFromText(text);
+
+            reply.edit(generateRollString(text, bonus, msg.id));
+        }
+    }
+
+    client.on('messageUpdate', callback);
+
+    setTimeout(() => {
+        client.off('messageUpdate', callback);
+    }, 60000)
+}
+
+module.exports = {
+    getBonusFromText,
+    getRollType,
+    generateRollString,
+    removeParenthesesFromString,
+    listenForMessageEdits
+}
